a3afb7796f51e11b0d10aa8652cae994a3aab99c
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-08 17:50:33 +0900

    itemsマイグレーションファイル の記述

---
+++ db/migrate/20210108083125_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,                    null: false, default: ""
+      t.text :info,                      null: false, default: ""
+      t.references :category,            null: false, foreign_key: true
+      t.references :sales_status,        null: false, foreign_key: true
+      t.references :shipping_fee_status, null: false, foreign_key: true
+      t.references :prefecture,          null: false, foreign_key: true
+      t.referencess :cheduled_delivery,  null: false, foreign_key: true
+      t.int :price,                      null: false, default: 0
+      t.references :sale_user,           null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
