839648ec2d88ae490aff251b5103fe89d61ba1f4
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-08 18:08:51 +0900

    textカラムにはdefaultを設定出来ないので修正

--- db/migrate/20210108083125_create_items.rb
+++ db/migrate/20210108083125_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,                    null: false, default: ""
-      t.text :info,                      null: false, default: ""
+      t.string     :name,                null: false, default: ""
+      t.text       :info,                null: false
       t.references :category,            null: false, foreign_key: true
       t.references :sales_status,        null: false, foreign_key: true
       t.references :shipping_fee_status, null: false, foreign_key: true
--- db/migrate/20210108083224_create_addresses.rb
+++ db/migrate/20210108083224_create_addresses.rb
@@ -4,8 +4,8 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string,     :postal_code,  null: false, default: ""
       t.references, :prefecture,   null: false, foreign_key: true
       t.string,     :city,         null: false, default: ""
-      t.text,       :addresses,    null: false, default: ""
-      t.text,       :building,     null: false, default: ""
+      t.text,       :addresses,    null: false
+      t.text,       :building,     null: false
       t.integer,    :phone_number, null: false
       t.references, :item,         null: false, foreign_key: true
       t.timestamps
