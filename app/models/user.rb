551d09d981e522462f899f35c901f949938bc001
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-09 06:40:11 +0900

    ユーザー登録にバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birth_date
+  end
   has_many :items,  dependent: :destroy
   has_many :orders, dependent: :destroy
 end
