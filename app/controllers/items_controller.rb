c4dd379e1748c0e665d25420b02747aa9460e4a5
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-10 10:15:13 +0900

    商品出品画面のルーティングとアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,7 @@ class ItemsController < ApplicationController
   def index
     
   end
+  def new
+    @item = Item.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  devise_for :users 
+  devise_for :users
+  resources :items , only: [:new]
 end
