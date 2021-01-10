d058f2de9f379b1f17d5fba8b866b55e9cdcd793
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-11 06:36:14 +0900

    商品一覧表示のルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
-  resources :items , only: [:new, :create]
+  resources :items , only: [:new, :create, :index, :show]
 end
