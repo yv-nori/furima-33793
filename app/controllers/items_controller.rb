29e4d2825886585fa23bb73d4b23906e137d3c8f
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-10 12:48:22 +0900

    出品アクションの定義とログイン認証

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
     
   end
   def new
     @item = Item.new
+  end
+
+  def create
+    @items = Item.new(items_params)
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(
+      :name, :info, :category_id,
+      :sales_status_id, :shipping_fee_status_id,
+      :prefecture_id, :scheduled_delivery_id,
+      :price).merge(sale_user_id: current_user.id)
   end
 end
