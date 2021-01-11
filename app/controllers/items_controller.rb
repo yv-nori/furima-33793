f94bade216b8f963cb575012f06457f3da7ce37f
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-11 09:30:41 +0900

    itemコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,13 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
   def new
     @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def create
